ac31a439bf650c585679dce0a5020a6d7b707396
Mohammed Elzanaty <megamas800@gmail.com>
2018-09-24 16:34:43 +0200

    make code style edit tslint Revert "edit tslint"
    This reverts commit c802fae21e264cdf27532f732dd2f77f7ccc7186.

--- angular-rest-app/src/app/shared/items.service.ts
+++ angular-rest-app/src/app/shared/items.service.ts
@@ -5,46 +5,39 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/switchMap';
-import {Injectable} from "@angular/core";
+import { Injectable } from '@angular/core';
 
 const BASE_URL = 'http://localhost:3000/items/';
-const HEADER = {headers: new Headers({'Content-Type': 'application/json'})};
+const HEADER = { headers: new Headers({ 'Content-Type': 'application/json' }) };
 
 @Injectable()
 export class ItemsService {
-  constructor(private http: Http) {
-  }
+  constructor(private http: Http) {}
 
   all() {
-    return this.http.get(BASE_URL)
-      .map(res => res.json());
+    return this.http.get(BASE_URL).map(res => res.json());
   }
 
   load(id) {
-    return this.http.get(`${BASE_URL}${id}`)
-      .map(res => res.json());
+    return this.http.get(`${BASE_URL}${id}`).map(res => res.json());
   }
 
   create(item: Item) {
-    return this.http.post(`${BASE_URL}`, JSON.stringify(item), HEADER)
-      .map(res => res.json());
+    return this.http.post(`${BASE_URL}`, JSON.stringify(item), HEADER).map(res => res.json());
   }
 
   update(item: Item) {
-    return this.http.patch(`${BASE_URL}${item.id}`, JSON.stringify(item), HEADER)
-      .map(res => res.json());
+    return this.http.patch(`${BASE_URL}${item.id}`, JSON.stringify(item), HEADER).map(res => res.json());
   }
 
   delete(item: Item) {
-    return this.http.delete(`${BASE_URL}${item.id}`)
-      .map(res => res.json());
+    return this.http.delete(`${BASE_URL}${item.id}`).map(res => res.json());
   }
 
   search(term: string) {
     const search = new URLSearchParams();
     search.set('q', term);
 
-    return this.http.get(`${BASE_URL}`, {search})
-      .map(res => res.json());
+    return this.http.get(`${BASE_URL}`, { search }).map(res => res.json());
   }
 }
