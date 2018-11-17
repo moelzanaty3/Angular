9485042b3e8b4375ff100f0862efca5eb7ffa4c6
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2018-11-17 11:25:46 +0200

    make routing for event component

---
+++ _fly_tuts/mean-rsvp/src/app/_core/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from '../_pages';
+
+const routes: Routes = [{ path: '', component: HomeComponent }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
