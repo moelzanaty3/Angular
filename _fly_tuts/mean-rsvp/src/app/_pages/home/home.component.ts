d30b94a196c0d280cb77f503a26b07b04e837764
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2018-11-17 11:26:15 +0200

    make home component

---
+++ _fly_tuts/mean-rsvp/src/app/_pages/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { TitleService } from './../../_services';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  pageTitle = 'Events';
+
+  constructor(private title: TitleService) {}
+
+  ngOnInit() {
+    this.title.setTitle(this.pageTitle);
+  }
+}
