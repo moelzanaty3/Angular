2d85897ead6b0137507e79d4a909db83a0bccc67
Mohammed <mohammedelzanaty129@gmail.com>
2019-01-16 17:07:03 +0200

    add pipe to data

--- deborah-kurata/ng:Getting_Started/Amazon/src/app/app.module.ts
+++ deborah-kurata/ng:Getting_Started/Amazon/src/app/app.module.ts
@@ -1,15 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { WelcomeComponent } from './home/welcome.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { StarComponent } from './shared/components/star/star.component';
+import { ConvertToSpacePipe } from './shared/pipes/convert-to-space.pipe';
 
 @NgModule({
-  declarations: [AppComponent, WelcomeComponent, ProductListComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    ProductListComponent,
+    StarComponent,
+    /* Pipes */
+    ConvertToSpacePipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
