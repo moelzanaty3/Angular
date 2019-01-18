2e622bc6b228462d0154e2a7b2e2609d164f6adc
Mohammed <mohammedelzanaty129@gmail.com>
2019-01-18 11:56:30 +0200

    init product services

--- deborah-kurata/ng:Getting_Started/Amazon/src/app/product-list/product-list.component.ts
+++ deborah-kurata/ng:Getting_Started/Amazon/src/app/product-list/product-list.component.ts
@@ -1,43 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { IProduct } from './interfaces/product';
+import { IProduct } from '../models/product';
+import { ProductsService } from '../services/products.service';
+ 
 
-const PRODUCTS: IProduct[] = [
-  {
-    productId: 1,
-    productName: 'Apple MacBook Air',
-    productCode: 'GDN-0011',
-    releaseDate: 'Jan 19, 2019',
-    description:
-      '13-inch Retina display, 1.6GHz dual-core Intel Core i5, 128GB) - Gold (Latest Model)',
-    price: 1149,
-    starRating: 3.6,
-    imageUrl: 'http://bit.ly/2M5JpbR'
-  },
-  {
-    productId: 2,
-    productName: 'ASUS C202SA-YS02 11.6',
-    productCode: 'GDN-0023',
-    releaseDate: 'Jan 18, 2019',
-    description:
-      'Ruggedized and Water Resistant Design with 180 Degree (Intel Celeron 4 GB, 16GB eMMC, Dark Blue, Silvers',
-    price: 218.99,
-    starRating: 4.2,
-    imageUrl:
-      'https://images-na.ssl-images-amazon.com/images/I/31inMpRxCFL._AC_US218_.jpg'
-  },
-  {
-    productId: 3,
-    productName: 'ASUS C302CA-DHM4',
-    productCode: 'TBX-0048',
-    releaseDate: 'May 21, 2019',
-    description:
-      'Chromebook Flip 12.5-inch Touchscreen Convertible Chromebook, Intel Core m3, 4GB RAM, 64GB Flash Storage, All-Metal Body, USB Type C, Corning Gorilla Glass, Chrome OS',
-    price: 474.99,
-    starRating: 4.8,
-    imageUrl:
-      'https://images-na.ssl-images-amazon.com/images/I/41LBkDN-S3L._AC_AA200_.jpg'
-  }
-];
 @Component({
   selector: 'pm-product-list',
   templateUrl: './product-list.component.html',
@@ -45,17 +10,21 @@ const PRODUCTS: IProduct[] = [
 })
 export class ProductListComponent implements OnInit {
   pageTitle = 'Product List';
-  products: IProduct[] = PRODUCTS;
+  products: IProduct[];
   imageWidth: number = 100;
   imageMargin: number = 10;
   exist: boolean = true;
   listFilter: string;
+  isFetched: boolean = false;
+  constructor(private productsService: ProductsService ) {}
 
-  startTime: any;
-  endTime: any;
-  constructor() {}
+  ngOnInit() {
+    setTimeout(()=> {
+      this.isFetched = !this.isFetched;
+      this.products = this.productsService.getProducts();
+    }, 2000)
+  }
 
-  ngOnInit() {}
   toggleImage(): void {
     this.exist = !this.exist;
   }
--- deborah-kurata/ng:Getting_Started/Amazon/src/app/shared/components/star/star.component.ts
+++ deborah-kurata/ng:Getting_Started/Amazon/src/app/shared/components/star/star.component.ts
@@ -9,7 +9,6 @@ export class StarComponent implements OnChanges {
   @Input() rating: number;
   starWidth: number;
   constructor() {}
-
   ngOnChanges(): void {
     this.starWidth = (this.rating * 75) / 5;
   }
